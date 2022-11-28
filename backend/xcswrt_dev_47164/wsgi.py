124e0786e2c4021d6b5ee65b0550ce553da6d01b
Crowdbotics <engineering@crowdbotics.com>
2022-11-28 09:47:46 +0000

    Initial commit.

---
+++ backend/xcswrt_dev_47164/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for xcswrt_dev_47164 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xcswrt_dev_47164.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "xcswrt-dev-47164",
+  url: "https://xcswrt-dev-47164.botics.co",
+  api: "https://xcswrt-dev-47164.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
